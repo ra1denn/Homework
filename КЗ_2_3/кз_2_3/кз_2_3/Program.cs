da53812b71a58920fcbfee6292f39c1b8fba3af0
Maria Ra1den <109177026+ra1denn@users.noreply.github.com>
2024-11-29 03:39:50 +0300

    Add files via upload

---
+++ КЗ_2_3/кз_2_3/кз_2_3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics.Metrics;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("ведите число n");
+        int n = Convert.ToInt32(Console.ReadLine());
+        int counter = 0;
+        while (n != 1)
+        {
+            if (n%2==0)
+            {
+                n /= 2;
+                counter++;
+            }
+
+            else
+            {
+                n = 3 * n + 1;
+                counter++;
+            }
+        }
+
+        Console.WriteLine("кол-во замен для достижения 1:" + counter);
+
+
+    }
+}
