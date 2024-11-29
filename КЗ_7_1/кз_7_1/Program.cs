b16a0fd8e3d96e3f286cdaeb6f704548b80d95cd
Maria Ra1den <109177026+ra1denn@users.noreply.github.com>
2024-11-29 05:12:57 +0300

    Add files via upload

---
+++ КЗ_7_1/кз_7_1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите число");
+
+        try
+        {
+            string input = Console.ReadLine();
+            int number = ConvertToInt(input);
+            Console.WriteLine($"число: {number}");
+        }
+
+        catch (FormatException)
+        {
+            Console.WriteLine("не число");
+        }
+
+        catch (OverflowException)
+        {
+            Console.WriteLine("число слишком большое или слишком маленькое");
+        }
+    }
+
+    static int ConvertToInt(string input)
+    {
+        return int.Parse(input);
+    }
+}
