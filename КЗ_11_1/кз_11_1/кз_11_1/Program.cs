b421e79379262425a05f1a555009ba10f8fa1d8e
Maria Ra1den <109177026+ra1denn@users.noreply.github.com>
2024-11-29 05:34:49 +0300

    Add files via upload

---
+++ КЗ_11_1/кз_11_1/кз_11_1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = "f.txt";
+
+        using (StreamWriter writer = new StreamWriter(filePath))
+        {
+            writer.WriteLine("x sin(x)");
+            for (double x = 0; x <= 1; x += 0.1)
+            {
+                double sinVal = Math.Sin(x);
+
+                writer.WriteLine($"{x:F1} {sinVal:F5}");
+            }
+        }
+
+        Console.WriteLine($"Таблица значений sin(x) записана в файл {filePath}");
+    }
+}
